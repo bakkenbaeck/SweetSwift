08288d4c0580e88e923749b8849e26a2297dfb9b
marijnschilling <marijnschilling@gmail.com>
2016-09-08 09:56:14 +0200

    Make the count a public var

--- Sources/RawRepresentable+Sweetness.swift
+++ Sources/RawRepresentable+Sweetness.swift
@@ -1,5 +1,5 @@
 extension RawRepresentable where RawValue: IntegerType {
-    static var count: Int {
+    public static var count: Int {
         var i: RawValue = 0
         while let _ = Self(rawValue: i) {
             i = i.successor()
