3020d36b8c140d4ec605705b5d9c613b28048a58
marijnschilling <marijnschilling@gmail.com>
2016-09-08 09:58:35 +0200

    Also make the extension public

--- Sources/RawRepresentable+Sweetness.swift
+++ Sources/RawRepresentable+Sweetness.swift
@@ -1,4 +1,4 @@
-extension RawRepresentable where RawValue: IntegerType {
+public extension RawRepresentable where RawValue: IntegerType {
     public static var count: Int {
         var i: RawValue = 0
         while let _ = Self(rawValue: i) {
