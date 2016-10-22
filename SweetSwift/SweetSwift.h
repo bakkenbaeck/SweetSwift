207339d107186c785e358b9164b1e2fe9240ed2f
Elvis Nuñez <elvisnunez@me.com>
2016-10-22 15:39:39 +0200

    Remove unused

--- SweetSwift/SweetSwift.h
+++ SweetSwift/SweetSwift.h
@@ -1,19 +1,2 @@
-//
-//  SweetSwift.h
-//  SweetSwift
-//
-//  Created by Igor Ranieri on 12/10/2016.
-//
-//
-
-#import <UIKit/UIKit.h>
-
-//! Project version number for SweetSwift.
 FOUNDATION_EXPORT double SweetSwiftVersionNumber;
-
-//! Project version string for SweetSwift.
 FOUNDATION_EXPORT const unsigned char SweetSwiftVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SweetSwift/PublicHeader.h>
-
-
