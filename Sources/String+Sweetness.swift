3e93752959cf76f346ce7b3208a12140c4989319
Elvis Nuñez <3lvis@users.noreply.github.com>
2016-11-17 15:16:06 +0100

    Adds String subscripting

--- Sources/String+Sweetness.swift
+++ Sources/String+Sweetness.swift
@@ -2,4 +2,19 @@ public extension String {
     public var length: Int {
         return self.characters.count
     }
+
+    public subscript(i: Int) -> String {
+        guard i >= 0 && i < characters.count else { return "" }
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+
+    subscript(range: Range<Int>) -> String {
+        let lowerIndex = index(startIndex, offsetBy: max(0,range.lowerBound), limitedBy: endIndex) ?? endIndex
+        return substring(with: lowerIndex..<(index(lowerIndex, offsetBy: range.upperBound - range.lowerBound, limitedBy: endIndex) ?? endIndex))
+    }
+
+    subscript(range: ClosedRange<Int>) -> String {
+        let lowerIndex = index(startIndex, offsetBy: max(0,range.lowerBound), limitedBy: endIndex) ?? endIndex
+        return substring(with: lowerIndex..<(index(lowerIndex, offsetBy: range.upperBound - range.lowerBound + 1, limitedBy: endIndex) ?? endIndex))
+    }
 }
