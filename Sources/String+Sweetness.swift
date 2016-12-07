6de9d4b06266ca8e90d9d2fc5174310872e8269b
marijnschilling <marijnschilling@gmail.com>
2016-12-07 14:31:58 +0100

    Add property that checks if string is a valid emailaddress

--- Sources/String+Sweetness.swift
+++ Sources/String+Sweetness.swift
@@ -1,5 +1,14 @@
+import Foundation
+
 public extension String {
     public var length: Int {
         return self.characters.count
+    }
+
+    public var isValidEmail: Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
     }
 }
