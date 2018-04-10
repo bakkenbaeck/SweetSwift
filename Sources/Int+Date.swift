8a73bac8062fdf707aa26a2acfa35840a72676e3
Igor Ranieri Elland <igor@elland.me>
2018-04-10 11:23:56 +0200

    Fixed extra 'r'.

--- Sources/Int+Date.swift
+++ Sources/Int+Date.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension Int {
     /// Returns a TimeInterval for an approximate number of days.
-    /// Ex: 3.days == 259200 seconds. Of courrse is not Date/Time aware. Use it with care.
+    /// Ex: 3.days == 259200 seconds. Of course is not Date/Time aware. Use it with care.
     ///
     /// Same as .days, but for the sake of grammatical order, we have both plural and singular.
     var day: TimeInterval {
@@ -17,7 +17,7 @@ extension Int {
     }
 
     /// Returns a TimeInterval for an approximate number of days.
-    /// Ex: 3.days == 259200 seconds. Of courrse is not Date/Time aware. Use it with care.
+    /// Ex: 3.days == 259200 seconds. Of course is not Date/Time aware. Use it with care.
     var days: TimeInterval {
         return TimeInterval(self * 60 * 60 * 24)
     }
