338279b7ccdf68040c4a812c015a0ddadaf25428
Ellen Shapiro <designatednerd@gmail.com>
2019-01-22 13:21:36 +0100

    Kill off deprecated count method

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -12,32 +12,6 @@ class Tests: XCTestCase {
         XCTAssertEqual(components.year, 2017)
     }
 
-    func testEnumCount() {
-        enum Enum1: Int {
-            case first
-            case second
-        }
-
-        XCTAssertEqual(Enum1.count, 2)
-
-        // The cases bellow are not supported by this extension
-        // if you have any idea on how to make them work in a good
-        // way, go for it.
-
-        enum Enum2: Int {
-            case first = 3
-            case second = 6
-        }
-
-        XCTAssertEqual(Enum2.count, 0)
-
-        enum Enum3: Int {
-            case first
-            case second = 6
-        }
-
-        XCTAssertEqual(Enum3.count, 1)
-    }
 
     func testStringLength() {
         let string = "twentyfour length string"
