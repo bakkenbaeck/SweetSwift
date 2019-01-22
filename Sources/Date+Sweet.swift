1e703cdc2c93a59e5a7f848fac22d29e663a8da9
Ellen Shapiro <designatednerd@gmail.com>
2019-01-22 13:22:34 +0100

    Update components-from-date method to take a time zone

--- Sources/Date+Sweet.swift
+++ Sources/Date+Sweet.swift
@@ -1,5 +1,11 @@
 public extension Date {
-    public func components(_ components: Set<Calendar.Component>, calendar: Calendar = .current) -> DateComponents {
-        return calendar.dateComponents(components, from: self)
+    
+    public func components(_ components: Set<Calendar.Component>,
+                           calendar: Calendar = .current,
+                           timeZone: TimeZone = .current) -> DateComponents {
+        var mutableCalendar = calendar
+        mutableCalendar.timeZone = timeZone
+        return mutableCalendar.dateComponents(components, from: self)
+    }
     }
 }
