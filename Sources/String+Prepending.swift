2bf432d68d88701c1d1f45a280ef84c163011064
Igor Ranieri Elland <igor@elland.me>
2019-05-24 11:10:54 +0200

    Added inline docs.

--- Sources/String+Prepending.swift
+++ Sources/String+Prepending.swift
@@ -8,10 +8,17 @@
 import Foundation
 
 extension String {
+    /// Prepends the given string to this string.
+    ///
+    /// - Parameter aString: string to be prepended
     public mutating func prepend(_ aString: String) {
         self = aString.appending(self)
     }
 
+    /// Returns a new string made by prepending a given string to the String.
+    ///
+    /// - Parameter aString: string to be prepended
+    /// - Returns: a new string composed of the combined strings.
     public func prepending(_ aString: String) -> String {
         return aString.appending(self)
     }
